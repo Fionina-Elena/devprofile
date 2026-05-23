fe7034169fd305f93decedade2ba4b1153e50ed6
Elena Fionina <filatowal2010@yandex.ru>
2026-05-23 23:40:51 +0400

    Исправлен контроллер

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -12,43 +12,46 @@ class ContactFormController extends Controller
         $validated = $request->validated();
         $apiKey = env('UNISENDER_API_KEY');
 
-        // Данные отправителя (должны совпадать с подтвержденными)
         $senderEmail = 'filatowa.l2010@yandex.ru';
         $senderName = 'Елена Фионина';
 
-        // Подготовка HTML
         $htmlOwner = view('emails.owner', ['data' => $validated])->render();
         $htmlUser = view('emails.user', ['data' => $validated])->render();
 
-        // Используем метод sendEmail согласно документации
-        $responseOwner = Http::asForm()->post('https://api.unisender.com/ru/api/sendEmail', [
-            'api_key' => $apiKey,
-            'email' => $senderEmail,
-            'sender_name' => $senderName,
-            'sender_email' => $senderEmail,
-            'subject' => 'Новое сообщение "devprofile"',
-            'body' => $htmlOwner,
-            'list_id' => 1, //  список ID
-            'format' => 'json',
-            'error_checking' => 1 // Рекомендация из документации
+        // --- КОД ДЛЯ UNISENDER GO ---
+        // 1. Отправка 
+        $responseOwner = Http::withHeaders([
+            'X-API-KEY' => $apiKey,
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json'
+        ])->post('https://go.unisender.com/ru/transactional/api/v1/email/send', [
+            "message" => [
+                "recipients" => [["email" => $senderEmail]],
+                "subject" => 'Новое сообщение "devprofile"',
+                "from_email" => $senderEmail,
+                "from_name" => $senderName,
+                "body" => [
+                    "html" => $htmlOwner
+                ]
+            ]
         ]);
 
         // 2. Отправка КЛИЕНТУ
-        $responseClient = Http::asForm()->post('https://api.unisender.com/ru/api/sendEmail', [
-            'api_key' => $apiKey,
-            'email' => $validated['email'],
-            'sender_name' => $senderName,
-            'sender_email' => $senderEmail,
-            'subject' => 'Спасибо за обращение',
-            'body' => $htmlUser,
-            'list_id' => 1,
-            'format' => 'json',
-            'error_checking' => 1
+        $responseClient = Http::withHeaders([
+            'X-API-KEY' => $apiKey,
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json'
+        ])->post('https://go.unisender.com/ru/transactional/api/v1/email/send', [
+            "message" => [
+                "recipients" => [["email" => $validated['email']]],
+                "subject" => 'Спасибо за обращение',
+                "from_email" => $senderEmail,
+                "from_name" => $senderName,
+                "body" => [
+                    "html" => $htmlUser
+                ]
+            ]
         ]);
-
-        // Логируем ответы для отладки
-        \Log::info('Unisender Owner: ' . $responseOwner->body());
-        \Log::info('Unisender Client: ' . $responseClient->body());
 
         return response()->json([
             'message' => 'success',
